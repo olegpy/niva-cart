c82d2c5db2ac8968ceb9767ff5c4acf440f07b7b
Oleg Kriklivets <olegkriklivets@gmail.com>
2025-06-10 20:10:50 -0400

    NIVA-4: config `jest`

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'jest';
-import nextJest from 'next/jest.js';
+import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
     // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -8,14 +8,24 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config: Config = {
-    coverageProvider: 'v8',
-    testEnvironment: 'jest-environment-jsdom',
-    // testEnvironment: 'jsdom',
-    // Add more setup options before each test is run
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jest-environment-jsdom',
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1', // according to folder structure will use alias
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            tsconfig: 'tsconfig.json',
+        }],
+    },
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/*.stories.{ts,tsx}',
+        '!src/**/*.test.{ts,tsx}',
+        '!src/**/index.{ts,tsx}',
+    ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
