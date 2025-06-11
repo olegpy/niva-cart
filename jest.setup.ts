cd629249e7a878ea7d37a9da23684b6eba3e51fa
Oleg Kriklivets <olegkriklivets@gmail.com>
2025-06-10 20:10:50 -0400

    NIVA-4: config `jest`

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,29 @@
 import '@testing-library/jest-dom';
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    return {
+      type: 'img',
+      props: {
+        ...props,
+        alt: props.alt || '',
+      },
+    };
+  },
+}));
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    };
+  },
+  usePathname() {
+    return '';
+  },
+}));
